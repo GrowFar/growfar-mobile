57b5528898238092ee39a70c344c405476ae0b60
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2020-08-10 10:03:51 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <SafeAreaView>
+        <Text>Hello There</Text>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default App;
