f0bf23c9d0fc752a704a7014588542ae2ce961f6
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2020-08-23 10:04:59 +0700

    Setup apollo client

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { ApolloProvider } from '@apollo/client';
+import client from './graphql/Client';
 import RegisterScreen from './screens/auth/RegisterScreen';
 import LoginScreen from './screens/auth/LoginScreen';
 import UserRegisterScreen from './screens/auth/UserRegisterScreen';
@@ -12,45 +14,47 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Register"
-          options={{ title: null, headerShown: false }}
-          component={RegisterScreen}
-        />
-        <Stack.Screen
-          name="Login"
-          options={{
-            title: null,
-            headerShown: true,
-            headerTransparent: true,
-          }}
-          component={LoginScreen}
-        />
-        <Stack.Screen
-          name="UserRegister"
-          options={{
-            title: null,
-            headerShown: true,
-            headerTransparent: true,
-          }}
-          component={UserRegisterScreen}
-        />
-        <Stack.Screen
-          name="ConfirmCode"
-          options={{
-            title: null,
-            headerShown: true,
-            headerTransparent: true,
-          }}
-          component={ConfirmCodeScreen}
-        />
-        <Stack.Screen
-          name="Home"
-          options={{ headerShown: false }}
-          component={HomeScreen}
-        />
-      </Stack.Navigator>
+      <ApolloProvider client={client}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Register"
+            options={{ title: null, headerShown: false }}
+            component={RegisterScreen}
+          />
+          <Stack.Screen
+            name="Login"
+            options={{
+              title: null,
+              headerShown: true,
+              headerTransparent: true,
+            }}
+            component={LoginScreen}
+          />
+          <Stack.Screen
+            name="UserRegister"
+            options={{
+              title: null,
+              headerShown: true,
+              headerTransparent: true,
+            }}
+            component={UserRegisterScreen}
+          />
+          <Stack.Screen
+            name="ConfirmCode"
+            options={{
+              title: null,
+              headerShown: true,
+              headerTransparent: true,
+            }}
+            component={ConfirmCodeScreen}
+          />
+          <Stack.Screen
+            name="Home"
+            options={{ headerShown: false }}
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </ApolloProvider>
     </NavigationContainer>
   );
 };
